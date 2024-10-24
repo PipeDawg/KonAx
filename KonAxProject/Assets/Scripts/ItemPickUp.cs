eec419d80da60b2911116fe3a40f00b56d94356a
bittemy <konradbuus.chr@gmail.com>
2024-10-24 18:26:45 +0200

    Made Player able to pick up items by walking on top of them

---
+++ KonAxProject/Assets/Scripts/ItemPickUp.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<Collider>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
