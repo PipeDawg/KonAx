0a3d8c50a1b1a8093009ae0cda0a334034782f94
PipeDawg <Axel.ahrensbach@gmail.com>
2024-10-26 19:58:27 +0200

    Added trail and fixed projectile not exploding on contact with non rigidbody gameobjects

--- KonAxProject/Assets/Scripts/Enemy/MageAttack.cs
+++ KonAxProject/Assets/Scripts/Enemy/MageAttack.cs
@@ -9,6 +9,7 @@ public class MageAttack : MonoBehaviour
 
     void Start()
     {
+        StartCoroutine(ProjectileExplosion(2f));
         _particleSystem = GetComponent<ParticleSystem>();
     }
 
@@ -22,19 +23,28 @@ public class MageAttack : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        StartCoroutine(Destroy());
         if (other.CompareTag("Player"))
         {
-            GetComponent<Light>().enabled = false;
-            GetComponent<MeshRenderer>().enabled = false;
-            _particleSystem.Play();
             other.GetComponentInParent<PlayerStats>().TakeDamage(Damage);
+            StartCoroutine(ProjectileExplosion(0));
+        }
+        else if (other.gameObject.name != "DetectionRange")
+        {
+            StartCoroutine(ProjectileExplosion(0));
         }
     }
 
-    IEnumerator Destroy()
+    IEnumerator ProjectileExplosion(float time)
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(time);
+        Explosion();
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
+    }
+
+    private void Explosion()
+    {
+        GetComponent<Light>().enabled = false;
         GetComponent<MeshRenderer>().enabled = false;
         _particleSystem.Play();
     }
