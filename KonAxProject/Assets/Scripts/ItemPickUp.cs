a6674c1717272c4db48675c877dee36d721763b3
bittemy <konradbuus.chr@gmail.com>
2024-10-27 13:20:49 +0100

    Player now has an inventory

--- KonAxProject/Assets/Scripts/ItemPickUp.cs
+++ KonAxProject/Assets/Scripts/ItemPickUp.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class ItemPickUp : MonoBehaviour
 {
+    [SerializeField] private int coinAmount;
+    
     private void Start()
     {
         GetComponent<Collider>().isTrigger = true;
@@ -11,6 +13,7 @@ public class ItemPickUp : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            other.GetComponentInParent<PlayerInventory>().coins += coinAmount;
             Destroy(gameObject);
         }
     }
